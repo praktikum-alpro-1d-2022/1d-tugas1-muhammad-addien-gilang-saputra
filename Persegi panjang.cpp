885fb0b6defd79e5b25732ed6c19a4f391b5a83a
AddienGilang <114645629+AddienGilang@users.noreply.github.com>
2022-10-12 19:44:16 +0700

    Add files via upload

---
+++ Persegi panjang.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double panjang, lebar, L;
+
+    cout<<"Masukan panjang: ";
+    cin >> panjang;
+
+    cout<<"masukan lebar: ";
+    cin >> lebar;
+
+    L = panjang*lebar;
+    cout<<"Luas persegi panjang: "<<L;
+
+    return 0;
+}
---
+++ Persegi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sisi, L;
+
+    cout<<"Masukan sisi: ";
+    cin >> sisi;
+
+    cout<<"masukan sisi: ";
+    cin >> sisi;
+
+    L = sisi*sisi;
+    cout<<"Luas persegi: "<<L;
+
+    return 0;
+}
